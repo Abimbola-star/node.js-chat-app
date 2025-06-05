a1a59bfb2d649ee16f675713de630bd8c63fd46d
Abimbola-star <aroyewunbimbola@gmail.com>
2025-06-05 15:55:15 -0500

    updated app.js

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Define route for the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', socket => {
     console.log('User connected');
